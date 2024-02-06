d0cf8b31e30a6f56b89babe42bfed55c9e283cef
Zaxarybinx <zackscherl@gmail.com>
2024-02-05 19:19:36 -0500

    added home component, router, radix theme

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Theme } from '@radix-ui/themes';
+import '@radix-ui/themes/styles.css';
+import Header from '../components/header/header';
+
+const Home = () => {
+  return (
+      <Theme>
+        <Header />
+      </Theme>
+  );
+};
+
+export default Home;
